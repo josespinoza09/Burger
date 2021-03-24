74c6810331584d823556134cce6044c7a5275b57
Jose Espinoza <jose20019004@gmail.com>
2021-03-24 10:31:49 -0400

    added the sql seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mcdouble', FALSE);
