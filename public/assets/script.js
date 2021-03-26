a34b1d009cb68d6cc6374eb9e25e2f4871ac9503
Jose Espinoza <jose20019004@gmail.com>
2021-03-26 13:11:17 -0400

    load function

--- public/assets/script.js
+++ public/assets/script.js
@@ -17,3 +17,25 @@ async function addBurger() {
   loadList();
 }
 
+async function loadList() {
+  const list = await fetch(`/burger`).then( r => r.json());
+  document.querySelector('#entries').innerHTML = '';
+
+  for (let i=0; i<list.length; i++) {
+    if (list[i].is_eaten) document.querySelector('#entries').innerHTML += 
+      `<div class="row-3"><div></div>
+      <div><button class="bgrBtn" data-fn="del" data-bid="${list[i].id}">Delete this burger</button></div>
+      <div>${list[i].burger_name}</div></div>`;
+    else document.querySelector('#entries').innerHTML += 
+      `<div class="row-3"><div>${list[i].burger_name}</div>
+      <div><button class="bgrBtn" data-fn="eat" data-bid="${list[i].id}" data-burger="${list[i].burger_name}">Eat the burger</button></div>
+      <div></div></div>`;
+  }
+
+  let btns = document.querySelectorAll(`.bgrBtn`);
+  btns.forEach(btn => btn.addEventListener('click', burgerBtn));
+}
+
+
+
+loadList();
